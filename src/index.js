52b3b783b2b8ac7e6cef9578bfe7476b5e75a3f9
Georg Burgstaller <gburgstaller@greentube.com>
2017-05-03 11:10:09 +0200

    added correct usage of combined reducers

--- src/index.js
+++ src/index.js
@@ -3,20 +3,32 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
 import { rootReducer } from './app/rootReducer';
+
+import counter from '../src/app/components/counter/index';
 import todos from '../src/app/components/todos/index';
 
 const { TodosActions, TodosComponent } = todos;
+const { CounterActions, CounterComponent } = counter;
+
 const store = createStore(rootReducer);
 
 const onAdd = () => store.dispatch(TodosActions.addTodo('test'));
+const onIncrement = () => store.dispatch(CounterActions.increment());
+const onDecrement = () => store.dispatch(CounterActions.decrement());
 
 const render = () => {
 
     const Todos = TodosComponent(React);
+    const Counter = CounterComponent(React);
 
     ReactDOM.render(
         <div>
-            <Todos todos={store.getState()} onAdd={onAdd} />
+            <Counter value={store.getState().CounterReducer} 
+                     onIncrement={onIncrement} 
+                     onDecrement={onDecrement} />
+
+            <Todos todos={store.getState().TodosReducer} 
+                   onAdd={onAdd} />
         </div>,
         document.getElementById('root')
     );
