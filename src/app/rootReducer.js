328a38e8946829e9c9b19de7068d65e0fe99d58c
Georg Burgstaller <gburgstaller@greentube.com>
2017-05-03 11:09:52 +0200

    added counter reducer

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import todos from './components/todos/index';
+import counter from './components/counter/index';
 
 const { TodosReducer } = todos;
+const { CounterReducer } = counter;
 
 export const rootReducer = combineReducers({
-    TodosReducer
+    TodosReducer,
+    CounterReducer
 });
