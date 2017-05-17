18741a4a01822e5ec129d05e9ee8e8114753ddb5
Georg Burgstaller <gburgstaller@greentube.com>
2017-05-17 10:08:17 +0200

    Fixed todos component

--- src/app/components/todos/todos.js
+++ src/app/components/todos/todos.js
@@ -1,11 +1,24 @@
+import * as TodosActions from './actions';
+
 export const TodosComponent = React => {
 
-    const createComponent = ({ todos, onAdd, textInput }) => {
+    const createComponent = ({ todos, store }) => {
+
+        let input;
 
         return (
             <div>
-                <input />
-                <button onClick={onAdd}>Add Todo</button>
+                <input ref={node => { input = node }} />
+                <button onClick={
+                    event => {
+                        event.preventDefault();
+
+                        if (!input.value.trim()) {
+                            return;
+                        }
+                        store.dispatch(TodosActions.addTodo(input.value));
+                    }
+                }>Add Todo</button>
                 <ul>
                     {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
                 </ul>
--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
 import { rootReducer } from './app/rootReducer';
-
 import counter from '../src/app/components/counter/index';
 import todos from '../src/app/components/todos/index';
 
-const { TodosActions, TodosComponent } = todos;
+const { TodosComponent } = todos;
 const { CounterActions, CounterComponent } = counter;
 
 const store = createStore(rootReducer);
 
-const onAdd = () => store.dispatch(TodosActions.addTodo('test'));
 const onIncrement = () => store.dispatch(CounterActions.increment());
 const onDecrement = () => store.dispatch(CounterActions.decrement());
 
@@ -23,12 +21,12 @@ const render = () => {
 
     ReactDOM.render(
         <div>
-            <Counter value={store.getState().CounterReducer} 
-                     onIncrement={onIncrement} 
-                     onDecrement={onDecrement} />
+            <Counter value={store.getState().CounterReducer}
+                onIncrement={onIncrement}
+                onDecrement={onDecrement} />
+            <br />
+            <Todos todos={store.getState().TodosReducer} store={store} />
 
-            <Todos todos={store.getState().TodosReducer} 
-                   onAdd={onAdd} />
         </div>,
         document.getElementById('root')
     );
@@ -36,3 +34,4 @@ const render = () => {
 
 store.subscribe(render);
 render();
+
